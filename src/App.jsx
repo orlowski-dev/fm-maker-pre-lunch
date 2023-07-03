4f004038b02b45a45815182a53ece3286fc63d03
Patryk Orłowski <orlowski.dev@gmail.com>
2023-07-03 21:28:15 +0200

    02 - Added about-service-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import './App.css';
+import './App.scss';
+import Logo from '/logo.svg';
+import ScrollIcon from './assets/icon-scroll.svg';
 
 export default function App() {
     return <>
-        <h1>Hello world</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quibusdam explicabo totam est rerum voluptatem quos saepe praesentium natus eum sint alias dicta, quod ab provident eos, omnis suscipit ad?</p>
+        <header>
+            <img src={Logo} alt="app logo" />
+        </header>
+
+        <main>
+            <section className="about-service-section">
+                <div className="img-container"></div>
+                <div className="content">
+                    <h1>Get paid for the work you <span>love</span> to do.</h1>
+                    <p>
+                        The 9-5 grind is so last century. We believe in living life on your
+                        own terms. Whether you’re looking to escape the rat race or set up
+                        a side hustle, we’ve got you covered.
+                    </p>
+                    <div className='scroll-icon-area'>
+                        <img src={ScrollIcon} alt="scroll icon" />
+                    </div>
+                </div>
+            </section>
+        </main>
     </>
 }
