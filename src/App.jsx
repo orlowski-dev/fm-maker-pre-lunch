0b1d41669afb4798c9afece5d72456c7361b94a0
Patryk Orłowski <orlowski.dev@gmail.com>
2023-07-03 08:49:37 +0200

    01 - Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+
+export default function App() {
+    return <>
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quibusdam explicabo totam est rerum voluptatem quos saepe praesentium natus eum sint alias dicta, quod ab provident eos, omnis suscipit ad?</p>
+    </>
+}
